224c2cf304c76e894ec2a95f8cb06a84b9265ea0
Greegko <greegko+github@gmail.com>
2022-12-04 13:28:37 +0100

    remove packages standalone compiles

--- packages/runner/webpack/webpack.config.js
+++ packages/runner/webpack/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 const basePath = path.resolve(__dirname, "../");
+const workspaceRoot = path.resolve(basePath, "../../");
+const tsConfigPath = path.resolve(workspaceRoot, "tsconfig.base.json");
+const tsconfig = require(tsConfigPath);
 
 module.exports = {
   entry: ["./src/main.tsx"],
@@ -16,6 +19,11 @@ module.exports = {
 
   resolve: {
     extensions: [".webpack.js", ".web.js", ".js", ".ts", ".tsx"],
+    alias: Object.keys(tsconfig.compilerOptions.paths).reduce((aliases, aliasName) => {
+      const pathBase = tsconfig.compilerOptions.paths[aliasName][0].replace(/\/\*$/, "");
+
+      return { ...aliases, [aliasName.replace(/\/*$/, "")]: path.resolve(workspaceRoot, pathBase) };
+    }, {}),
   },
 
   module: {
@@ -27,7 +35,7 @@ module.exports = {
           {
             loader: "ts-loader",
             options: {
-              configFile: "../tsconfig.base.json",
+              configFile: tsConfigPath,
             },
           },
         ],
@@ -35,6 +43,10 @@ module.exports = {
       {
         test: /\.scss$/i,
         use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        resourceQuery: /\?url$/,
+        type: "asset/resource",
       },
     ],
   },
