af050e409dd62113c271649ac1e1873f99d00fd7
Greegko <greegko+github@gmail.com>
2022-12-13 00:34:22 +0100

    fix function usage

--- packages/mods/src/mods/castle-wars/resource-manager.ts
+++ packages/mods/src/mods/castle-wars/resource-manager.ts
@@ -1,12 +1,12 @@
-import { find, prop } from "ramda";
+import { find, propEq } from "ramda";
 
 import { ResourceManager as IResourceManager, UnitConfig } from "@battleground/core";
 
-const units = require("./units.json");
-const buildings = require("./buildings.json");
+const units = require("./units.json") as UnitConfig[];
+const buildings = require("./buildings.json") as UnitConfig[];
 
 export class ResourceManager implements IResourceManager {
   getUnitConfig(unitId: string): UnitConfig {
-    return find(prop("id", unitId), units) || find(prop("id", unitId), buildings);
+    return find(propEq("id", unitId), units) || find(propEq("id", unitId), buildings);
   }
 }
